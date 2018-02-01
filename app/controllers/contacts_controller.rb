cf4f302ee350685bbadedd8ca41251692b6df3a8
benjamin <benjamin.balanger@epitech.eu>
2018-02-01 19:27:32 +0100

    Removing delete

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -17,45 +17,6 @@ class ContactsController < ApplicationController
     @all_valid_contacts = Contact.valid_contact
     @all_invalid_contacts = Contact.invalid_contact
   end
-  # # POST /contacts
-  # # POST /contacts.json
-  # def create
-  #   @contact = Contact.new(contact_params)
-  #
-  #   respond_to do |format|
-  #     if @contact.save
-  #       format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
-  #       format.json { render :show, status: :created, location: @contact }
-  #     else
-  #       format.html { render :new }
-  #       format.json { render json: @contact.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # PATCH/PUT /contacts/1
-  # PATCH/PUT /contacts/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @contact.update(contact_params)
-  #       format.html { redirect_to @contact, notice: 'Contact was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @contact }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @contact.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # DELETE /contacts/1
-  # DELETE /contacts/1.json
-  def destroy
-    @contact.destroy
-    respond_to do |format|
-      format.html { redirect_to contacts_url, notice: 'Contact was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
